d4f3c1a0b339c209abf0e52fa5730dc9b417de27
Sage Master <sheru.sherki28@gmail.com>
2022-04-19 18:04:54 +0530

    updated ui to load books after entering index

--- lib/pages/brs_home.dart
+++ lib/pages/brs_home.dart
@@ -1,5 +1,6 @@
 import 'package:book_search/controller/bookList_controller.dart';
 import 'package:book_search/controller/brs_controller.dart';
+import 'package:book_search/models/book_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,15 +12,112 @@ class BrsHome extends StatelessWidget {
     final BookListController bookListController = Get.put(BookListController());
     final BrsController brsController = Get.put(BrsController());
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            bookListController.fetchBooks();
-            // brsController.fetchRecommendations(51);
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.grey.shade900,
+        title: Container(
+          height: 38,
+          child: TextField(
+            onSubmitted: (value) async {
+              value.replaceAll(" ", "+");
+              value.toLowerCase();
+              String val =
+                  await brsController.fetchRecommendations(int.parse(value));
+              print(value);
+            },
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.amber,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[850],
+                contentPadding: EdgeInsets.all(0),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey.shade500,
+                ),
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none),
+                hintStyle: TextStyle(fontSize: 14, color: Colors.grey.shade500),
+                hintText: "Enter Index"),
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.black87,
+        child: Obx(
+          () {
+            if (brsController.isLoading.value) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Center(
+                child: ListView.builder(
+                  itemCount: brsController.bookList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (brsController.bookList.isEmpty) {
+                      return const Center(
+                        child: Text("Please enter a search term"),
+                      );
+                    } else {
+                      return BrsBookCard(book: brsController.bookList[index]);
+                      // return Text(
+                      //     searchController.bookList[index].title.toString());
+                    }
+                  },
+                ),
+              );
+            }
           },
-          child: const Text("Retrieve Books"),
         ),
       ),
     );
   }
 }
+
+class BrsBookCard extends StatelessWidget {
+  const BrsBookCard({
+    Key? key,
+    required this.book,
+  }) : super(key: key);
+
+  final BrsBook book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          color: Colors.black87,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Image.network(
+                  book.imgUrl.toString(),
+                ),
+                Text(
+                  book.title.toString(),
+                  style: const TextStyle(
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Author = " + book.author.toString(),
+                  style: subStyles,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+TextStyle subStyles = TextStyle(color: Colors.amber);
